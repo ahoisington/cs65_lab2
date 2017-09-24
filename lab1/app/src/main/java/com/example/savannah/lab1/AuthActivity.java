8cdc0ca80bc2bf0fa53fe6dc4808f3c261a445c3
savannah <savannah.m.liu.18@dartmouth.edu>
2017-09-24 15:20:40 -0400

    save button at bottom

---
+++ lab1/app/src/main/java/com/example/savannah/lab1/AuthActivity.java
@@ -0,0 +1,9 @@
+package com.example.savannah.lab1;
+
+/**
+ * This activity handles the pop up for password confirmation
+ * Based on the activity example in http://www.cs.dartmouth.edu/~sergey/cs65/examples/Dialog/
+ */
+
+public class AuthActivity {
+}
--- lab1/app/src/main/java/com/example/savannah/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/savannah/lab1/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.savannah.lab1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *   This activity holds the bulk of the UI for creating a user profile
+ *   Based on the activity example in http://www.cs.dartmouth.edu/~sergey/cs65/examples/Dialog/
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
