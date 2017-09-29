b7d5f1ba9a003fb3e968303ce9c6609195de2086
savannah <savannah.m.liu.18@dartmouth.edu>
2017-09-29 00:08:46 -0400

    added imagebutton for picture (temporary default pic)

--- lab1/app/src/main/java/com/example/savannah/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/savannah/lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.savannah.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 /**
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // sets toolbar as the app bar for the activity
+
     }
 
     // Lifecycle instrumentation
